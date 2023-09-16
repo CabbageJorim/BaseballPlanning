acc48dfc6bd3a957e5911fcbc11accaff1175651
JorimJoram <wkdgyfla97@gmail.com>
2023-09-16 15:39:59 +0900

    feat: service and unit test - need to change CSV files - commit

--- src/main/kotlin/com/plan/baseball/model/service/CrawlingService.kt
+++ src/main/kotlin/com/plan/baseball/model/service/CrawlingService.kt
@@ -4,28 +4,31 @@
 package com.plan.baseball.model.service
 
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
-import org.springframework.stereotype.Service
 
 class CrawlingService(
     private var teamURL: String
 ) {
-    /**
-     * 사용안 할 가능성 높은 메서드
-     */
-    fun crawlingTeam(): String{
-        return try{
-            val doc: Document = Jsoup.connect(this.teamURL).get()
-            val tables: List<Element> = doc.select("table")
-            tables.joinToString(separator = "") {
-                table -> table.select(".fixed").toString()
-            }
-        }catch (e:Exception){
-            "none"
-        }finally {
-            println("Crawling Fin")
+    private fun getTrsInTable(tableList: List<Element>): List<Element> {
+        var trList:List<Element> = emptyList()
+        for(table in tableList){
+            trList = table.select("tr")
         }
+        return trList
     }
 
+    fun crawlingTeamTableOptions():String{
+        var result = ""
+        try{
+            val doc = Jsoup.connect(this.teamURL).get()
+            val tableList: List<Element> = doc.select("table")
+            for(tr in getTrsInTable(tableList)){
+                result += tr.select("td").text()
+            }
+        }catch (_:Exception){
+
+        }.toString()
+
+        return result
+    }
 }
---
+++ src/test/kotlin/com/plan/baseball/CrawlingTest.kt
@@ -0,0 +1,16 @@
+package com.plan.baseball
+
+import com.plan.baseball.model.service.CrawlingService
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrawlingTest(
+
+) {
+    @Test
+    fun crawlingTeam(){
+        val crawlingService = CrawlingService("http://www.gameone.kr/club/info/ranking/hitter?club_idx=14106")
+        println(crawlingService.crawlingTeamTableOptions())
+    }
+}
